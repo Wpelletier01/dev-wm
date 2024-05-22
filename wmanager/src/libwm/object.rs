29406b5857149b073c1a7f45bae7d9e741be6b98
William Pelletier <wip000@hpcs-test-03.science.gc.ca>
2024-05-22 20:25:50 +0000

    add object

--- wmanager/src/libwm/object.rs
+++ wmanager/src/libwm/object.rs
@@ -1,6 +1,31 @@
 
-#[derive(Debug)]
+#[derive(Debug,Clone)]
 pub struct Window {
-    pub class: String,
+    id:         u32,
+    title:      String,
+    workspace:  u8,
+    processed:  bool
+}
+
+impl Window {
+
+
+    pub fn new(id:u32, workspace:u8, title:String) -> Self {
+        Self {
+            id,
+            workspace,
+            title,
+            processed: false
+        }
+    }
+
+    pub fn is_processed(&self) -> bool { self.processed }
+    pub fn get_workspace(&self) -> u8 { self.workspace }
+    pub fn get_title(&self) -> &str { &self.title }
+    pub fn get_xid(&self) -> u32 { self.id }
+
+    pub fn set_process(&mut self,p:bool) { self.processed = true }
 
 }
+
+
