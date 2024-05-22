3fa5c8cba92b2acdcb3e6499bc6d64e283b07ff9
William Pelletier <wip000@hpcs-test-03.science.gc.ca>
2024-05-22 20:25:17 +0000

    add simple logic

--- wmanager/src/bin.rs
+++ wmanager/src/bin.rs
@@ -1,5 +1,7 @@
 use anyhow::Result;
 use std::env;
+use std::thread;
+use std::time::Duration;
 
 fn main() -> Result<()> {
 
@@ -7,9 +9,30 @@ fn main() -> Result<()> {
     env::set_var("DISPLAY", "localhost:0");
 
     let interactor = libwm::Interactor::new()?;
+  
+    let mut manager = libwm::Manager::new(interactor.get_window_on_screen(0, 1)?, 1);
 
-    for win in interactor.get_window_on_screen(0)?.iter() {
-        println!("{:?}",win);
+    let (screen_w,screen_h) = interactor.get_screen_dim();
+    let mut windows:Vec<libwm::object::Window>;
+
+    loop {
+
+        windows = manager.get_windows();
+
+        for window in windows.iter_mut() {
+            if !window.is_processed() {
+                let (w,h) = manager.process_window_size(window.get_xid(), screen_w,screen_h);
+                
+                let _ = interactor.update_window_geo(window.get_xid(), 0, 0, w, h)?;
+                
+                window.set_process(true);
+
+            }
+        }
+
+        manager.set_windows(windows);
+
+        thread::sleep(Duration::from_secs(1));
     }
 
     Ok(())
